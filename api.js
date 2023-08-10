83f44952a4f9996e5f9dce95d7611e5fc3a80068
extrajake <extrajelly@live.ca>
2023-08-11 02:08:32 +1000

    test distributor

--- api.js
+++ api.js
@@ -5,11 +5,11 @@ document.getElementById('codeForm').addEventListener('submit', function(event) {
   // Get the value from the input field
   var url = document.getElementById('inputField').value;
 
-  const codes = ["1NKCI", "V3L7R", "60HPC", "8SF6L", "AHG08"];
-  const codes2 = ["2WEMV", "KWWNI", "XNB7X", "6BN2T", "Y2FOK"];
-  const codes3 = ["YJYZL", "6HNP2", "V9FF8", "8SF5L", "5B23I"];
-  const codes4 = ["UVUNU", "1N5UL", "IINVK", "AEZNP", "HM2NM"];
-  const codes5 = ["KGFIA", "XI28K", "2QQ8K", "9Y4HK", "DMQJT"];
+  const codes = ["DCC1000"];
+  const codes2 = ["DCC2000"];
+  const codes3 = ["DCC3000"];
+  const codes4 = ["DCC4000"];
+  const codes5 = ["DCC5000"];
 
   if (codes.includes(url)) {
     window.location.href = '/promo1.html'
