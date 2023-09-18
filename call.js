9734464990a0fafea117908b82e56f890bc77525
extrajake <extrajelly@live.ca>
2023-09-19 00:42:57 +1000

    changes to promo tile pages

--- call.js
+++ call.js
@@ -1,24 +1,88 @@
-// Replace 'YOUR_API_KEY' with your actual HubSpot API key
-const apiKey = 'pat-na1-70302af9-f0f7-4814-96cd-f3648b8f4eca';
+// // Make the API call
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
 
-// Define the HubSpot API endpoint for companies
-const endpoint = 'https://api.hubapi.com/companies/v2/companies/paged';
+// const id = urlParams.get("id");
+// var data = localStorage.getItem("data");
+// var headers = {
+//   "QB-Realm-Hostname": "jakelacouvee.quickbase.com",
+//   Authorization: "QB-USER-TOKEN b7kbbw_qsqq_0_b9ykyiacmnx3bbj6z9gjqmupkr",
+//   "Content-Type": "application/json"
+// };
+// var body = {
+//   from: "btkjt6r8g",
+//   select: [ 1, 2, 3, 4, 5, 9, 10],
+//   where: "{42.EX.'" + id + "'}"
+// };
 
-// Define a function to fetch companies
-async function fetchCompanies() {
-  try {
-    const response = await fetch(`${endpoint}?hapikey=${apiKey}`);
-    const data = await response.json();
-    
-    // Process and display the list of companies
-    const companies = data.companies;
-    companies.forEach(company => {
-      console.log(company.name);
-    });
-  } catch (error) {
-    console.error('Error fetching companies:', error);
-  }
+// const xmlHttp = new XMLHttpRequest();
+
+// xmlHttp.open('GET', 'https://api.quickbase.com/v1/fields/10?tableId=btkjt6r8g', true);
+
+// for (const key in headers) {
+//     xmlHttp.setRequestHeader(key, headers[key]);
+// }
+
+// xmlHttp.onreadystatechange = function() {
+//     if (xmlHttp.readyState === XMLHttpRequest.DONE) {
+//         const response = JSON.parse(xmlHttp.response);
+//         if (response.data) {
+//             var item = response.data[0];
+//             console.log(item);
+
+//             var phoneNumber = (item[9].value) ? item[9].value : "";
+//             var dccCode = (item[10].value) ? item[10].value : "";
+//             document.getElementById("phoneNumber").innerHTML = phoneNumber;
+//             document.getElementById("dccCode").innerHTML = dccCode;
+//         }
+//     }
+
+
+// }
+// xmlHttp.send(JSON.stringify(body));
+
+// Make the API call
+
+
+var headers = {
+  'QB-Realm-Hostname': 'jakelacouvee.quickbase.com',
+'User-Agent': '{User-Agent}',
+'Authorization': 'QB-USER-TOKEN b7kbbw_qsqq_0_b9ykyiacmnx3bbj6z9gjqmupkr',
+  'Content-Type': 'application/json'
 }
 
-// Call the fetchCompanies function to retrieve and display companies
-fetchCompanies();
+// var body = {
+//   from: "btkjt6r8g",
+//   select: [9, 10],
+//   where: "{42.EX.'" + id + "'}"
+// };
+
+
+const xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', 'https://api.quickbase.com/v1/fields/10?tableId=btkjt6r8g', true);
+for (const key in headers) {
+xmlHttp.setRequestHeader(key, headers[key]);
+}
+xmlHttp.onreadystatechange = function() {
+if (xmlHttp.readyState === XMLHttpRequest.DONE) {
+  const responseObj = JSON.parse(xmlHttp.response);
+
+  if (responseObj.data) {
+                var item = response.data[0];
+                console.log(item);
+    
+                var phoneNumber = (item[9].value) ? item[9].value : "";
+                var dccCode = (item[10].value) ? item[10].value : "";
+                document.getElementById("phoneNumber").innerHTML = phoneNumber;
+                document.getElementById("dccCode").innerHTML = dccCode;
+            }
+
+  console.log(responseObj);
+      
+      // Now you can work with the responseObj as a JavaScript object
+  
+  }
+};
+
+xmlHttp.send();
+
